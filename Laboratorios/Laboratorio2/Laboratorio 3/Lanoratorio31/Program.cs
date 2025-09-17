f81a6fbc5aeb96cc7278a74b00ec5440cc5a23ff
Enrique Lajon <enrique.lajon@utp.ac.pa>
2025-09-16 20:49:44 -0500

    LAB 3 LISTO

---
+++ Laboratorios/Laboratorio2/Laboratorio 3/Lanoratorio31/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero, multiplicacion;
+
+        Console.Write("Introduce el primer numero: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo numero: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        multiplicacion = (primerNumero + segundoNumero) * (primerNumero - segundoNumero);
+
+        Console.WriteLine("El resultado de la operacion es de: {0}", multiplicacion);
+    }
+}
