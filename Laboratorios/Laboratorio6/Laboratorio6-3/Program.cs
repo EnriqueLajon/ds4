537a4734e32742e0eeaa830df0626aca855ea0e5
Enrique Lajon <enrique.lajon@utp.ac.pa>
2025-09-23 20:59:30 -0500

    lab6 listo

---
+++ Laboratorios/Laboratorio6/Laboratorio6-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]); // Esto generará IndexOutOfRangeException
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal, valide el índice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuacion de la aplicacion, luego del bloque try/catch");
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorios/Laboratorio6/Laboratorio6-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Accesso negado - No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Acceso Concedido");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            checkAge(15);
+        }
+        catch (ArithmeticException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.ReadKey();
+    }
+}
